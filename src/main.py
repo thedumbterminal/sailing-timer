38e438613bf518243504f2c0440af4862024dc34
thedumbterminal <github@thedumbterminal.co.uk>
2026-06-10 11:29:33 +0100

    add start button

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 
 class SailingApp(App):
-    def build(self):
-        return Label(text='Sailing app')
+    def build(self):    
+        layout = BoxLayout(padding=10, orientation='vertical')
+        title = Label(text='Sailing app')
+        startButton = Button(text='Start')
+        layout.add_widget(title)
+        layout.add_widget(startButton)
+        return layout
 
 if __name__ == '__main__':
     SailingApp().run()
