d1476233db2de14f94a2c6734800e639de62fa4f
thedumbterminal <github@thedumbterminal.co.uk>
2026-06-10 11:14:50 +0100

    first files

---
+++ script/setup-python.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+PYTHONVERSION=`cat .python-version | grep -E -o "^[^/]+"`
+VIRTUALENVSHORT=`cat .python-version | grep -E -o "[^/]+$"`
+VIRTUALENVFULL=`cat .python-version`
+PYENV_VERSION=${PYTHONVERSION}
+pyenv virtualenv-delete -f ${VIRTUALENVSHORT}
+pyenv virtualenv ${PYTHONVERSION} ${VIRTUALENVFULL}
+unset PYENV_VERSION
+pyenv version
