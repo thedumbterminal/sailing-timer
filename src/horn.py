f77bb1f0b5c7f3f5ffd505f4d3036936150f51ab
thedumbterminal <github@thedumbterminal.co.uk>
2026-07-05 20:51:42 +0100

    now supporting gpio pins

--- src/horn.py
+++ src/horn.py
@@ -1,9 +1,23 @@
 from kivy.logger import Logger
+from gpiozero import Device, LED
+from gpiozero.pins.mock import MockFactory
+from time import sleep
+import platform
+
+if platform.system() == "Darwin":
+    from gpiozero.pins.mock import MockFactory
+
+    Device.pin_factory = MockFactory()
 
 
 class Horn:
     def __init__(self):
+        self._pin = LED(17)
         Logger.debug("Horn: Created")
 
     def sound(self):
+        Logger.debug("Horn: Sounding")
+        self._pin.on()
+        sleep(5)
+        self._pin.off()
         Logger.debug("Horn: Sounded")
